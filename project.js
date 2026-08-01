fbfd3fa7f77da474ce60189128976d3437dd9323
Yashita Gaur <yashitagaur72@gmail.com>
2026-08-01 12:24:38 +0530

    font update

--- project.js
+++ project.js
@@ -126,7 +126,7 @@ function buildProjectsHTML() {
 
         <div class="pv-actions">
           <a href="contact.html#inquiryForm" class="pv-btn pv-btn-primary">
-            <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z"/></svg>
+            <svg viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
             View Details
           </a>
           <a href="contact.html" class="pv-btn pv-btn-outline">
